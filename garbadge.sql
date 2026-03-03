00f633331a89f34d7265cc13a64f5216ce533927
dthang-candies <dag.thag.205@gmail.com>
2026-03-03 10:43:48 +0700

    update

--- garbadge.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-        f_table_schema AS schema_name,
-        f_table_name AS table_name,
-        f_geometry_column AS geom_column,
-        type,
-        srid
-      FROM public.geometry_columns
-      WHERE f_table_schema NOT IN ('pg_catalog', 'information_schema')
-      ORDER BY f_table_schema, f_table_name
